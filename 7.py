4d1f2f37ce5a1a1b065460cfff1ad9f6c109e034
manuelantoniodavila <134649893+manuelantoniodavila@users.noreply.github.com>
2024-10-12 21:47:27 -0600

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,20 +1,18 @@
-#7.	Realice una función recursiva de búsqueda secuencial, 
-# la función devuelve el índice donde se encontró el  elemento buscado. 
 
-def DevuelveIndice (lista, dato, indice=0):            
+def DevuelveElIndice (lista, dato, indice=0):            
     if indice >= len(lista):
         return -1
     
     if lista[indice] == dato:
         return indice
-    return DevuelveIndice(lista, dato, indice + 1)
+    return DevuelveElIndice(lista, dato, indice + 1)
 
 lista = [10, 20, 30,40 ,50]
 dato = 30
-resultado = DevuelveIndice (lista, dato)
+resultado = DevuelveElIndice (lista, dato)
 
 if resultado != -1:
-    print(f"El número {dato} se encuentra en : {resultado}")
+    print(f"El número de {dato} se encuentra en {resultado}")
 else:
-    print(f"El número {dato} NO se encuentra en la lista.")
+    print(f"El número de {dato} NO se encuentra en la lista")
     
