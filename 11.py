a2246fa662f926cf13750e27b4d595da3d22e612
manuelantoniodavila <134649893+manuelantoniodavila@users.noreply.github.com>
2024-10-12 21:58:55 -0600

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,14 +1,13 @@
-#11.Una función recursiva que retorne la suma de las longitudes de las cadenas de caracteres almacenados en un arreglo
 
 
-def LlenarLista(lista, n, i=0):
+def LlenarLaLista(lista, n, i=0):
     if (n==i):
-        numero = int(input("Ingrese el numero: "))
+        numero = int(input("Ingrese los numeros"))
         lista.append(numero)
     else:
-        numero = int(input("Ingrese el número: "))
+        numero = int(input("Ingrese los número"))
         lista.append(numero)  # Agregar el número a la lista
-        LlenarLista(lista, n, i + 1)
+        LlenarLaLista(lista, n, i + 1)
         
 def SumaDeLongitudes(lista, i ):
     if(len(lista))== i:
@@ -18,13 +17,13 @@ def SumaDeLongitudes(lista, i ):
         return lista[i] + SumaDeLongitudes (lista, i+1)
     
     
-n = int(input("Ingrese la cantidad de elementos a llenar: "))
+n = int(input("Ingrese la cantidad de elementos a ingresar "))
 
 lista = []
-LlenarLista (lista, n, 1)
+LlenarLaLista (lista, n, 1)
 
 suma = SumaDeLongitudes(lista, 0)
 
-print(f" La suma de las longitudes de las cadenas de caracteres almacenados es: {suma}")
+print(f" La suma de las longitudes de las cadenas de caracteres es: {suma}")
 
     
