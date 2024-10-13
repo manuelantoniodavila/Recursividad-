17065fccf0ba9aed813f8f81f49f3c1f4e64d816
manuelantoniodavila <134649893+manuelantoniodavila@users.noreply.github.com>
2024-10-12 22:00:50 -0600

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,30 +1,27 @@
-#12.Se tiene un arreglo de N elementos de tipo enteros, 
-# realice una función recursiva que retorne la suma de
-# los elementos almacenado en las casillas pares del 
-# arreglo. 
+ 
 
-def LlenarLista(lista, n, i):
+def LlenarLaLista(lista, n, i):
     if (n==i):
-        numero = int(input("Ingrese el numero: "))
+        numero = int(input("Ingrese el numero "))
         lista.append(numero)
     else:
-        numero = int(input("Ingrese el número: "))
-        lista.append(numero)  # Agregar el número a la lista
-        LlenarLista(lista, n, i + 1)
+        numero = int(input("Ingrese el número "))
+        lista.append(numero)  
+        LlenarLaLista(lista, n, i + 1)
         
-def SumaPare (lista, i):
+def SumaDeLosPares(lista, i):
     if (len(lista) == i):
         return 0
     if (lista[i] % 2 == 0):
-        return lista[i] + SumaPare (lista, i+1)
+        return lista[i] + SumaDeLosPares (lista, i+1)
     else:
-        return SumaPare(lista, i+1)
+        return SumaDeLosPares(lista, i+1)
     
-n = int(input("Ingresse la cantidad del elemento a llenar: "))
+n = int(input("Ingresse la cantidad del elemento a llenar"))
 
 lista = []
-LlenarLista (lista, n, 1)
-suma = SumaPare (lista, 0)
+LlenarLaLista (lista, n, 1)
+suma = SumaDeLosPares (lista, 0)
 
 
-print (f" La Suma de los elementos alamacenados Pares es: {suma}")
+print (f" La Suma de los elementos Pares es: {suma}")
