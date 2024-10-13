d6515c700a3f872c9b1ad6198219e9c653524986
manuelantoniodavila <134649893+manuelantoniodavila@users.noreply.github.com>
2024-10-12 22:12:23 -0600

    Ej5.py

--- CadenaInversa.py
+++ CadenaInversa.py
@@ -1,15 +1,15 @@
-# Funcion Recursiva
-def ImprimirCadenaInversa(cadena, i):
-    if(i == 0): # Donde Finaliza
-        print(cadena[i])
+
+def verificarEimprimir(frase, index=0):
+    if index == len(frase):
+        print(frase)  
+        return
     
-    else: # Llamado Recursivo
-        print(cadena[i])
-        ImprimirCadenaInversa(cadena, i-1)
-        
+    if frase[index].lower() == 'a':
+        print("La frase contiene la vocal 'a', no se puede imprimir")
+        return
+    
+    verificarEimprimir(frase, index + 1)
 
-# Ejecucion principal del programa
-cadena = input("Ingrese cadena a invertir: ")
+frase = input("Ingrese una frase: ")
 
-# Ejecucion de Funcion Recursiva
-ImprimirCadenaInversa(cadena, len(cadena)-1)
+verificarEimprimir(frase)
