baf6d2ed01f18e9d3e673912ea229ee36ac730b9
manuelantoniodavila <134649893+manuelantoniodavila@users.noreply.github.com>
2024-10-12 21:57:24 -0600

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,32 +1,31 @@
-#Una función recursiva que retorne la suma de los elementos impares del arreglo. 
 
-def LlenarLista(lista, n, i=0):
+def LlenarLaLista(lista, n, i=0):
     if (n==i):
-        numero = int(input("Ingrese el numero: "))
+        numero = int(input("Ingrese los numeros "))
         lista.append(numero)
     else:
-        numero = int(input("Ingrese el número: "))
-        lista.append(numero)  # Agregar el número a la lista
-        LlenarLista(lista, n, i + 1)
+        numero = int(input("Ingrese los números "))
+        lista.append(numero)  
+        LlenarLaLista(lista, n, i + 1)
         
         
-def SumaImpares(lista, i):
+def SumaDeImpares(lista, i):
     if(len (lista))==i:
         return 0
         
     else:
         if (lista[i] %2 !=0):
-            return lista[i] + SumaImpares (lista, i+1)
+            return lista[i] + SumaDeImpares (lista, i+1)
         else:
-            return 0 + SumaImpares (lista, i+1)
+            return 0 + SumaDeImpares (lista, i+1)
 
     
-n = int(input("Ingrese la cantidad de elementos a llenar: "))
+n = int(input("Ingrese la cantidad de elementos que desea llenar"))
 
 lista = []
-LlenarLista (lista, n, 1)
+LlenarLaLista (lista, n, 1)
 
-suma = SumaImpares(lista, 0)
+suma = SumaDeImpares(lista, 0)
 
-print(f"La suma de los elementos impares es: {suma}")
+print(f"La suma de los elementos impares es{suma}")
     
