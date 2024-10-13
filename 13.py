41de6e1dbd64f2bc0b0e5ba8109ef94ab203abae
manuelantoniodavila <134649893+manuelantoniodavila@users.noreply.github.com>
2024-10-12 22:02:53 -0600

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,15 +1,14 @@
-#Una función recursiva que retorne el mayor de los 
-# elementos de un arreglo de enteros. 
 
-def LlenarLista (lista, n, i):
+
+def LlenarLaLista (lista, n, i):
     if (n==i):
-        numero = int(input("Ingrese el numero: "))
+        numero = int(input("Ingrese el numero"))
         lista.append(numero)
     else:
         LlenarLista
-        numero = int(input("Ingrese el numero: "))
+        numero = int(input("Ingrese el numero "))
         lista.append(numero)
-        LlenarLista(lista, n, i + 1)
+        LlenarLaLista(lista, n, i + 1)
         
 def MayorElmento (lista, i=0, mayor=None):
     if (len(lista)==i):
@@ -22,11 +21,11 @@ def MayorElmento (lista, i=0, mayor=None):
         mayor = lista[i]            
     return MayorElmento(lista, i + 1, mayor)
         
-n = int(input("Ingrese la cantidad del elemento a llenar: "))
+n = int(input("Ingrese la cantidad del elemento a llenar "))
 
 lista = []
-LlenarLista (lista, n, 1)
+LlenarLaLista (lista, n, 1)
 
 mayor = MayorElmento(lista)
 
-print (f" El Mayor de los elementos es: {mayor}")
+print (f" El Mayor de los elementos encontrado es{mayor}")
