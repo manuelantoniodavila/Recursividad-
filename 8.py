43b7e3c1cc900c637912dc5301c83f40020a24f9
manuelantoniodavila <134649893+manuelantoniodavila@users.noreply.github.com>
2024-10-12 21:53:36 -0600

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,15 +1,14 @@
-#8.	Una función recursiva que retorne  cuantos ceros hay en un arreglo de enteros.
 
-def ContarCero (lista, indice=0):
+def ContarNumeroCero (lista, indice=0):
     if indice >= len(lista):
         return 0
     
     if lista[indice] == 0:
-        return 1 + ContarCero (lista, indice +1 )
+        return 1 + ContarNumeroCero (lista, indice +1 )
     else:
-        return ContarCero(lista, indice + 1)
+        return ContarNumeroCero(lista, indice + 1)
     
 arreglos = [8,1,3,5,3,0,1,6,8,4,0,7,4,0,5,0,53,0,2,0,0,5,7,43,0,43,0]
-resultado = ContarCero(arreglos)
+resultado = ContarNumeroCero(arreglos)
 
-print(f"La cantidad de ceros en los arreglos es: {resultado}")
+print(f"La cantidad de ceros es: {resultado}")
